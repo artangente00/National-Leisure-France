aa2077e14f3c36972d6008ff8c0a2370b257172c
artangente00 <98647236+artangente00@users.noreply.github.com>
2022-09-07 00:46:41 +0800

    Update page.php
    unable sidebars

--- page.php
+++ page.php
@@ -19,14 +19,14 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 ?>
 
-<div class="wrapper" id="page-wrapper">
+<div class="wrapper" id="page-wrapper" style="height:1000px; border:1px solid black">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
 		<div class="row">
 
 			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+			<!-- <php get_template_part( 'global-templates/left-sidebar-check' ); ?> -->
 
 			<main class="site-main" id="main">
 
@@ -71,7 +71,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			</main><!-- #main -->
 
 			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+			<!-- <php get_template_part( 'global-templates/right-sidebar-check' ); ?> -->
 
 		</div><!-- .row -->
 
